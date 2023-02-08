fbafd6889d02180cd92f005322a5a899256f3ab7
punishedchumbucket6000 <nathanieljamesallen2@gmail.com>
2023-02-07 22:31:08 -0500

    changes

--- main.py
+++ main.py
@@ -1,4 +1,73 @@
 from dijsktra import Graph
+
+def run_united_states():
+  
+  
+  # if list element distance is the same then discard
+  
+  edges = [
+    ("LA","SLC",222),
+    ("LA","LV",169),
+    ("LA", "FL", 676),
+    ("LV","FL", 99),
+    ("LV","NY",617),
+    ("LV","CH",254),
+    ("LV","VC",327),
+    ("LV","SL",69),
+    ("VC","NY",603),
+    ("VC","BO",561),
+    ("MN","NY",237),
+    ("CH","NY",226),
+    ("FL","NY",299),
+    ("FL","BO",465)
+
+      ]
+    
+  graph = Graph(edges)
+  start = 'LA'
+  stop = 'CH'
+  path = graph.dijsktra_shortest_path(start, stop)
+  print (path)
+  print ("There is/are " + str(len(path)-2) + " layover(s)")
+  i=0
+  total_cost=0
+  while i < len(path):
+    total_cost += edges[0][2]
+    i+=1 
+  print("The cost is $" + str(total_cost))
+  
+  
+
+def run_romania():
+  edges = [
+    ("Ar", "Si",140),
+    ("Ar","Ti",118),
+  ]
+
+  graph = Graph(edges)
+  start = 'A'
+  stop = 'E'
+  path = graph.dijsktra_shortest_path(start, stop)
+  print (path)
+
+def run_video_graph():
+  edges = [ 
+    ("A","B",6),
+    ("A","D",1),
+    ("D","E",1),
+    ("B","E",2),
+    ("E", "C",5),
+    ("B", "C",5),
+    ("D","B", 2),
+
+    ]
+  graph = Graph(edges)
+  start = 'A'
+  stop = 'E'
+  path = graph.dijsktra_shortest_path(start, stop)
+  print (path)
+
+
     
 def run_sample():
   edges = [
@@ -30,7 +99,10 @@ def run_sample():
   
 
 def main():
-  run_sample()
+  #run_sample()
+  #run_video_graph()
+  #run_romania()
+  run_united_states()
 
 if __name__ == "__main__":
   main()
